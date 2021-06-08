f8842b363ca2c95ca2529776c5e09a855da7191a
Hetav Patel <hetav2014@gmail.com>
2021-06-08 13:11:30 +0530

    first commit!

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Header from "./Header";
+import CovApi from "../api/CovApi";
+
+const App = () => {
+
+    return (
+        <div className="appContainer" >
+            <Header />
+        </div>
+    );
+};
+
+export default App;
